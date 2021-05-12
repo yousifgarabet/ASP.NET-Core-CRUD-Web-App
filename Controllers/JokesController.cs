0ca7bf64e6b175f9074ec84eec1ef22a904dd8c9
Yousif Garabet <yousifgarabet@gmail.com>
2021-05-12 14:05:24 +0300

    Add Authorization

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ASP.NET_Core_CRUD_Web_App.Data;
 using ASP.NET_Core_CRUD_Web_App.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ASP.NET_Core_CRUD_Web_App.Controllers
 {
@@ -33,7 +34,7 @@ namespace ASP.NET_Core_CRUD_Web_App.Controllers
         // Post: Search Result
         public async Task<IActionResult> SearchResult(string SearchJoke)
         {
-            return View("Index",await _context.Joke.Where(J => J.JokeQuestion.Contains(SearchJoke)).ToListAsync());
+            return View("Index", await _context.Joke.Where(J => J.JokeQuestion.Contains(SearchJoke)).ToListAsync());
         }
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -54,6 +55,7 @@ namespace ASP.NET_Core_CRUD_Web_App.Controllers
         }
 
         // GET: Jokes/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -62,6 +64,7 @@ namespace ASP.NET_Core_CRUD_Web_App.Controllers
         // POST: Jokes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -76,6 +79,7 @@ namespace ASP.NET_Core_CRUD_Web_App.Controllers
         }
 
         // GET: Jokes/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -94,6 +98,7 @@ namespace ASP.NET_Core_CRUD_Web_App.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -127,6 +132,7 @@ namespace ASP.NET_Core_CRUD_Web_App.Controllers
         }
 
         // GET: Jokes/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -145,6 +151,7 @@ namespace ASP.NET_Core_CRUD_Web_App.Controllers
         }
 
         // POST: Jokes/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
