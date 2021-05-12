02acfedd3b7ad1b73bb7d962fd7cd3c287c83623
Yousif Garabet <yousifgarabet@gmail.com>
2021-05-12 10:33:49 +0300

    add database

--- Data/Migrations/20210512072803_initialsetup.Designer.cs
+++ Data/Migrations/20210512072803_initialsetup.Designer.cs
@@ -4,14 +4,16 @@ using ASP.NET_Core_CRUD_Web_App.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASP.NET_Core_CRUD_Web_App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210512072803_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
